5cc2fbba785a7cd5103b414dea30e2038645c476
andrea.campostrini <andrea.campostrini@marconirovereto.it>
2023-04-29 22:06:07 +0200

    Quick fix image 8k to 4k, and cursor animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { AppIntroType } from './app-intro/app-intro-type';
-import { ClickMode, Container, Engine, HoverMode, ISourceOptions, MoveDirection, OutMode } from 'tsparticles-engine';
-import { particlesOptions } from './login/particles-config';
-import { loadFull } from 'tsparticles';
 
 @Component({
   selector: 'app-root',
