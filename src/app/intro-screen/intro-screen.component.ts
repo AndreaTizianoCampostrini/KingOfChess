e84dc0cd2fbbb30c7ab184c616752fecc1bd04a5
andrea.campostrini <andrea.campostrini@marconirovereto.it>
2023-05-02 23:19:36 +0200

    fix mouse in deployment

--- src/app/intro-screen/intro-screen.component.ts
+++ src/app/intro-screen/intro-screen.component.ts
@@ -1,28 +1,12 @@
-import { Component, HostListener, OnDestroy } from '@angular/core';
-import { CursorService } from '../cursor.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-intro-screen',
   templateUrl: './intro-screen.component.html',
   styleUrls: ['./intro-screen.component.css']
 })
-export class IntroScreenComponent implements OnDestroy {
+export class IntroScreenComponent {
 
-  constructor(private cursorService: CursorService){
-  }
-
-  onMouseEnter() {
-    this.cursorService.addClassToCursor(document.querySelector('.cursor'), 'cursor-hover');
-    this.cursorService.changeCursor('../../assets/cursor-black.png');
-  }
-
-  onMouseLeave() {
-    this.cursorService.removeClassFromCursor(document.querySelector('.cursor'), 'cursor-hover');
-    this.cursorService.changeCursor('../../assets/cursor-white.png');
-  }
-
-  ngOnDestroy() {
-    this.cursorService.removeClassFromCursor(document.querySelector('.cursor'), 'cursor-hover');
-    this.cursorService.changeCursor('../../assets/cursor-white.png');
+  constructor(){
   }
 }
