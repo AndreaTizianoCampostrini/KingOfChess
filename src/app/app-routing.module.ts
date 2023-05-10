45fc89bf7ce80696a81008d4ef9671e71e361fe0
andrea.campostrini <andrea.campostrini@marconirovereto.it>
2023-05-11 01:21:16 +0200

    intro add, ng build, fix loader position

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '', component: IntroScreenComponent },
+  { path: 'intro', component: IntroScreenComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard]  },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '/404' }
